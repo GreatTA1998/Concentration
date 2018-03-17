8c62a685a4fdc69c99d7d18387aa8a3c983deeb5
Elton Lin <eltonlin@soundvillages.com>
2018-03-17 15:34:04 -0400

    refactor with protocols and extensions

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -18,15 +18,29 @@ class ViewController: UIViewController
     }
     
     private(set) var flipCount = 0 {
-        didSet {
-            flipCountLabel.text = "Flips: \(flipCount)"
+        didSet { // initialization does not invoke didSet
+            updateFlipCountLabel()
         }
     }
     
-    @IBOutlet private weak var flipCountLabel: UILabel!
+    private func updateFlipCountLabel() {
+        let attributes: [NSAttributedStringKey:Any] = [
+            .strokeWidth : 5.0,
+            .strokeColor : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        ]
+        let attributedString = NSAttributedString(string: "Flips: \(flipCount)", attributes: attributes)
+        flipCountLabel.attributedText = attributedString
+    }
+    
+    @IBOutlet private weak var flipCountLabel: UILabel! {
+        didSet {
+            updateFlipCountLabel()
+        }
+    }
+    
     @IBOutlet private var cardButtons: [UIButton]!
-    private var emojiChoices = ["👽", "🎃", "👻", "🍔", "🐌", "🐙", "🦑", "🐡"]
-    private var emoji = [Int:String]()
+    private var emojiChoices = "👽🎃👻🍔🐌🐙🦑🐡"
+    private var emoji = [Card:String]()
     
     @IBAction private func touchCard(_ sender: UIButton) {
         flipCount += 1
@@ -53,10 +67,11 @@ class ViewController: UIViewController
     }
 
     private func emoji(for card: Card) -> String {
-        if emoji[card.identifier] == nil, emojiChoices.count > 0 {
-            emoji[card.identifier] = emojiChoices.remove(at: emojiChoices.count.arc4random)
+        if emoji[card] == nil, emojiChoices.count > 0 {
+            let randomStringIndex = emojiChoices.index(emojiChoices.startIndex, offsetBy: emojiChoices.count.arc4random)
+            emoji[card] = String(emojiChoices.remove(at: randomStringIndex))
         }
-        return emoji[card.identifier] ?? "?"
+        return emoji[card] ?? "?"
     }
     
     private func flipCard(withEmoji emoji: String, on button: UIButton) {
@@ -68,7 +83,6 @@ class ViewController: UIViewController
             button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         }
     }
-    
 }
 
 extension Int {
